4b1eb85ec957991184d240a54ea620134f1f831f
Amber Bai <bai.lu3838@gmail.com>
2020-07-22 17:12:29 -0400

    new product form

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::ProductsController < ApiController
+  # protect_from_forgery unless: -> { request.format.json? }
+
+  def create 
+    new_product = Product.new(product_params)  
+    if new_product.save
+      render json: new_product
+    else
+      render json: {errors: new_product.errors.full_messages}
+    end
+  end
+
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :url, :image_url, :description)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   root 'products#index'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :products, only: [:index]
+  
+  get '/products/new', to: "homes#index"
+  
+  namespace :api do
+    namespace :v1 do
+      resources :products, only: [:index, :create]
+    end 
+  end 
 
 end
