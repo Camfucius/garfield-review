a243549a5046bbe8a872da02db6120282ba22d83
Amber Bai <bai.lu3838@gmail.com>
2020-07-24 16:35:40 -0400

    set review database

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :url, presence: true
   validates :description, presence: true
+  
+  has_many :reviews
 end
---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  validates :rating, presence: true
+
+  belongs_to :product
+  belongs_to :user
+end
